71532ed27d44a91742b848d253e9787dde67fa82
Oleh <musiychuk09@gmail.com>
2025-01-29 17:47:39 +0200

    Create models in models.py:
    Create models in models.py: Airport, Route, Flight, Crew, Airplane, AirplaneType, Ticket, Order

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    closest_big_city = models.CharField(max_length=100)
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="sours_routes"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="destination_route"
+    )
+    distance = models.IntegerField()
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        "Airplane",
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crews = models.ManyToManyField(
+        "Crew",
+        related_name="flights"
+    )
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        "AirplaneType",
+        on_delete=models.CASCADE,
+        related_name="airplane_types"
+    )
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+class Order(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=100)
