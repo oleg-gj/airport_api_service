3afdd437ed1022c737f71f49f3e4ac3e9548d52c
Oleh <musiychuk09@gmail.com>
2025-02-04 14:17:46 +0200

    Create serializers.py with serializers
    Create serializers.py with serializers: AirportSerializer, RouteSerializer, RouteListSerializer, CrewSerializer, FlightSerializer, FlightListSerializer, AirplaneSerializer, AirplaneListSerializer, AirplaneTypeSerializer, TicketSerializer, TicketListSerializer, OrderSerializer, OrderListSerializer

---
+++ airport/serializers.py
@@ -0,0 +1,150 @@
+from django.db import transaction
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from airport.models import (
+    Airport,
+    Route,
+    Flight,
+    Crew,
+    AirplaneType,
+    Ticket,
+    Order,
+    Airplane
+)
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "closest_big_city")
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = (
+            "id",
+            "source",
+            "destination",
+            "distance",
+        )
+
+
+class RouteListSerializer(RouteSerializer):
+    source = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    destination = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+        )
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+            "crews"
+        )
+
+
+class FlightListSerializer(FlightSerializer):
+    route = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    airplane = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    crews = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "airplane_type",
+            "capacity"
+        )
+
+
+class AirplaneListSerializer(AirplaneSerializer):
+    airplane_type = serializers.SlugRelatedField(
+        slug_field="name", read_only=True
+    )
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["flight"].airplane,
+            ValidationError
+        )
+        return data
+
+    class Meta:
+        model = Ticket
+        fields = (
+            "id",
+            "row",
+            "seat",
+            "flight",
+            "order"
+        )
+
+
+class TicketListSerializer(TicketSerializer):
+    flight = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    order = serializers.SlugRelatedField(slug_field="name", read_only=True)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "created",
+            "tickets"
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
+
+class OrderListSerializer(OrderSerializer):
+    user = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    tickets = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True,)
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "created",
+            "user",
+            "tickets"
+        )
+
