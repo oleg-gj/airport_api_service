1b6fc25ca65204b79ec5ece52fdd2dbacc01435a
Oleh <musiychuk09@gmail.com>
2025-02-04 16:27:34 +0200

    Add unique=True for name in Airport in models.py

--- airport/models.py
+++ airport/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Airport(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     closest_big_city = models.CharField(max_length=100)
 
 
