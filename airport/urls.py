a97bed5cd1e335608f95c6adb0e67c5aab98f4f7
Oleh <musiychuk09@gmail.com>
2025-02-04 16:14:48 +0200

    Register routs in airport/urls.py
    Register routs for: AirportViewSet, RouteViewSet, FlightViewSet, CrewViewSet, AirplaneViewSet, AirplaneTypeViewSet, TicketViewSet, OrderViewSet

--- airport/urls.py
+++ airport/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from airport.views import (
+    AirportViewSet,
+    RouteViewSet,
+    AirplaneViewSet,
+    AirplaneTypeViewSet,
+    TicketViewSet,
+    OrderViewSet,
+    CrewViewSet,
+    FlightViewSet
+)
+
 router = routers.DefaultRouter()
+router.register("airport", AirportViewSet)
+router.register("route", RouteViewSet)
+router.register("airplane", AirplaneViewSet)
+router.register("airplane_type", AirplaneTypeViewSet)
+router.register("ticket", TicketViewSet)
+router.register("order", OrderViewSet)
+router.register("crew", CrewViewSet)
+router.register("flight", FlightViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
