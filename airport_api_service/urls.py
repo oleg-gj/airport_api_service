c582c82100ba9052eaf378ef639c380f096d13cf
Oleh <musiychuk09@gmail.com>
2025-02-16 17:37:41 +0200

    Add debug_toolbar_urls()

--- airport_api_service/urls.py
+++ airport_api_service/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.contrib import admin
 from django.urls import path, include
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -38,4 +40,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-]
+] + debug_toolbar_urls()
